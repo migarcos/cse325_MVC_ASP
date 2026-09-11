07fb3af12bdc3df74bdb4db10b1ecb1570c90c2c
migarcos <migark@hotmail.com>
2026-09-11 11:24:41 -0500

    Working with SQL

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7785a4d9d4c6ad7edcf20675783ce14687d080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9c8dd049680d7fad3f173e7c5d53dd3082ccdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
