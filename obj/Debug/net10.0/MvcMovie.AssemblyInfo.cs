519e437a10de6bf26acd5e96189cbe3b3fb7f4c2
migarcos <migark@hotmail.com>
2026-09-11 09:41:33 -0500

    scaffolding

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+790af7f57354d84019d6b95213b4cd372a794438")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d586341ccf7743e35cb5f7c7bdaca7e4737be489")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
