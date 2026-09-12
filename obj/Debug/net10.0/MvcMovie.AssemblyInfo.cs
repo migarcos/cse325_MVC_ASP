1b874434bfb5d0061446b2c2e5404d97890b9393
migarcos <migark@hotmail.com>
2026-09-11 21:36:15 -0500

    Validation + Additonal 5

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcbb6d4257fdbf91a71ecfcdc3eb1cad1104917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07c85a50d5d327e4dfa5c2ef36f032c206f76b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
