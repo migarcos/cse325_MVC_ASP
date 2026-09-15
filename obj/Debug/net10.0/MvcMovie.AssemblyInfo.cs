7c9c659f7b952f5a0d3b23bbae395cc7719325a5
migarcos <migark@hotmail.com>
2026-09-15 08:09:59 -0500

    Movies added

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b874434bfb5d0061446b2c2e5404d97890b9393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8dd60aa0ddaf76f4b01da81a29effe0d5c89bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
